9c3a373193274f781d6e91343e60b12158a3d92e
BuildTools <unconfigured@null.spigotmc.org>
2016-11-05 22:39:33 +0100

    great start man

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -5,14 +5,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
 
+	private static DevathonPlugin instance;
+	
     @Override
     public void onEnable() {
+    	instance = this;
         Bukkit.getServer().getLogger().info("this might work holy shit fuck me right");
+        Bukkit.getPluginManager().registerEvents(new FridgeListener(), this);
     }
 
     @Override
     public void onDisable() {
-        //pls
+    	instance = null;
     }
+
+	public static DevathonPlugin getInstance() {
+		return instance;
+	}
+	
 }
 
---
+++ src/main/java/org/devathon/contest2016/FridgeListener.java
@@ -0,0 +1,7 @@
+package org.devathon.contest2016;
+
+import org.bukkit.event.Listener;
+
+public class FridgeListener implements Listener {
+
+}
